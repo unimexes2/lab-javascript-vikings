d3f5c81f0b523e03d0da663d9647bf5f7d07e001
unimexes2 <unimexes2@gmail.com>
2022-06-06 17:54:53 +0200

    first itteration

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,125 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(healthProp, strengthProp) {
+
+    this.health = healthProp;
+    this.strength = strengthProp;
+
+  }
+  attack() {
+    return this.strength
+
+
+  }
+  receiveDamage(damage) {
+
+    this.health = this.health - damage;
+
+
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+  constructor(nameProp, healthProp, strengthProp,) {
+
+    super(healthProp, strengthProp)
+    this.name = nameProp;
+  }
+
+
+
+  receiveDamage(damage) {
+
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      let dam = this.name + " has received " + damage + " points of damage";
+
+      return dam;
+
+    } else { return this.name + " has died in act of combat" }
+
+  }
+  battleCry() {
+    return "Odin Owns You All!"
+
+
+  }
+
+
+
+
+
+
+
+
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  receiveDamage(damage) {
+
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      let dam = "A Saxon has received " + damage + " points of damage";
+
+      return dam;
+
+    } else { return "A Saxon has died in combat" }
+
+  }
+
+
+
+
+
+
+
+
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+
+
+  }
+
+
+  addViking(obj) {
+    this.vikingArmy.push(obj);
+  }
+
+
+  addSaxon(obj) {
+    this.saxonArmy.push(obj)
+
+  }
+
+
+  vikingAttack() {
+
+    let saxon=this.saxonArmy[0]
+    let viking=this.vikingArmy[0]
+    saxon.receiveDamage(viking.strength);
+    
+  }
+
+
+  //saxonAttack()
+  //showStatus()
+
+
+
+
+}
 
 
 
